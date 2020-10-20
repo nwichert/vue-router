e17fedc4a22766769b412bb22b3336f703e9dee6
Nick Wichert <nickwichert@nicks-mbp.lan>
2020-10-19 21:44:29 -0500

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
